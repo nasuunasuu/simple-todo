4a07f2968566906e59569b140d99f9457ab4ba06
nasuunasuu <nasuu0612@gmail.com>
2024-03-28 14:21:15 +0900

    [add]リストをロードする機能と細かい修正

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
     loadLists()
 })
 
-function addList(){
-    var listInput=document.getElementById('listInput')
-    var listContent=listInput.value.trim()
+function addList() {
+    var listInput = document.getElementById('listInput')
+    var listContent = listInput.value.trim()
 
-    if(listContent!==""){
-        var ul=document.getElementById('lists')
-        var li =document.createElement('li')
+    if (listContent !== "") {
+        var ul = document.getElementById('lists')
+        var li = document.createElement('li')
 
-        li.textContent=listContent
+        li.textContent = listContent
         ul.appendChild(li)
-        listInput.value=""
+        listInput.value = ""
         saveLists()
     }
 }
 
-function saveLists(){
-    var lists=[];
-    var lis=document.querySelectorAll('#list li')
-    lis.forEach(()=>{
+function saveLists() {
+    var lists = [];
+    var lis = document.querySelectorAll('#lists li')
+    lis.forEach(li => {
         lists.push({
-            list:  li.textContent
+            list: li.textContent
         })
     })
-    localStorage.setItem('lists',JSON.stringify(lists))
+    localStorage.setItem('lists', JSON.stringify(lists))
+}
+
+
+function loadLists() {
+    var lists = JSON.parse(localStorage.getItem('lists')) || []
+    var ul = document.getElementById('lists')
+    lists.forEach((list) => {
+        var li = document.createElement('li')
+        li.textContent = list.list
+
+        ul.appendChild(li)
+    })
+
 }
